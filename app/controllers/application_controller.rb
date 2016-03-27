5b04d58e61f00abc72bb283f3817b8e00b1b21c9
Giniouxe <aurelie.ginioux@gmail.com>
2016-03-27 19:28:24 +0200

    Setup locales from urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
+# Application controller
 class ApplicationController < ActionController::Base
+  before_filter :set_current_locale
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -6,5 +8,11 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
+  end
+
+  private
+
+  def set_current_locale
+    I18n.locale = params[:locale]
   end
 end
